d7186c6a9406ea9e651c6bd4494962ae250407f2
Douglas <drmcclus@ncsu.edu>
2014-01-21 08:24:08 -0500

    changing xcat_site table to abstract xcat_resource description
    using XCat's resource management model with lsdef, chdef, mkdef & rmdef

--- lib/puppet/type/xcat_site.rb
+++ lib/puppet/type/xcat_site.rb
@@ -1,57 +1,27 @@
 # This has to be a separate type to enable collecting
-Puppet::Type.newtype(:xcat_site) do
+Puppet::Type.newtype(:xcat_resource) do
   @doc = 'Manage the xcat site table.'
 
   newparam(:name, :namevar=>true) do
-    desc 'Does nothing.'
+    desc 'XCat Resource name'
   end
 
-  newproperty(:installdir) do
-    desc 'Install directory.'
+  newparam(:type) do
+    desc 'XCat Resource type: object or table'
   end
-
-  newproperty(:forwarders) do
-    desc 'DNS Forwarders.'
-  end
-
-  newproperty(:dhcpinterfaces) do
-    desc 'DHCP Interfaces.'
-  end
-
-  newproperty(:master) do
-    desc 'IP of master node.'
-  end
-
-  newproperty(:nameservers) do
-    desc 'IPs of nameservers (default: master node).'
-  end
-
-  newproperty(:nodesyncfiledir) do
-    desc 'Directory for node syncing.'
-  end
-
-  newproperty(:tftpdir) do
-    desc 'TFTP directory.'
-  end
-
-  newproperty(:xcatport) do
-    desc 'XCat Port.'
-  end
-
-  newproperty(:xcatiport) do
-    desc 'XCat IPort.'
-  end
-
-  newproperty(:timezone) do
-    desc 'Timezone.'
-  end
-
-  newproperty(:sshbetweennodes) do
-    desc 'SSH Between Nodes setting.  See XCat documentation for valid settings.'
-  end
-
-  newproperty(:dhcplease) do
-    desc 'lease time for the dhcp client.'
+  
+  newproperty(:value, :array_matching => :all) do
+    desc 'Array of values for this XCat Resource'
+    def insync?(is)
+      # The current value may be nil and we don't
+      # want to call sort on it so make sure we have arrays 
+      # (@ref https://ask.puppetlabs.com/question/2910/puppet-types-with-array-property/)
+      if is.is_a?(Array) and @should.is_a?(Array)
+        is.sort == @should.sort
+      else
+        is == @should
+      end
+    end
   end
 
 end
