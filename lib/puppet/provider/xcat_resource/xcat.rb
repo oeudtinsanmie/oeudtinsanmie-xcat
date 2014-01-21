6de9395678e6181ba30382bdc30ff715060306b1
Douglas <drmcclus@ncsu.edu>
2014-01-21 08:37:02 -0500

    Update xcat.rb

--- lib/puppet/provider/xcat_resource/xcat.rb
+++ lib/puppet/provider/xcat_resource/xcat.rb
@@ -1,1 +1,8 @@
+Puppet::Type(:xcat_resource).provide(:xcat) do
   
+  commands  :lsdef => '/opt/xcat/bin/lsdef',
+            :mkdef => '/opt/xcat/bin/mkdef',
+            :rmdef => '/opt/xcat/bin/rmdef',
+            :chdef => '/opt/xcat/bin/chdef'
+            
+end
