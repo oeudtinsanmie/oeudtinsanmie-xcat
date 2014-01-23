02a5f7ca96cbc469259087b71d090ab436f729bd
Douglas McClusky <drmcclus@ncsu.edu>
2014-01-23 06:52:28 -0500

    creating superclasses for xcat puppet providers

---
+++ lib/puppet/provider/xcat_object.rb
@@ -0,0 +1,8 @@
+class Puppet::Provider::xcat_object < Puppet::Provider
+
+  commands  :lsdef => '/opt/xcat/bin/lsdef',
+            :mkdef => '/opt/xcat/bin/mkdef',
+            :rmdef => '/opt/xcat/bin/rmdef',
+            :chdef => '/opt/xcat/bin/chdef'
+            
+end
---
+++ lib/puppet/provider/xcat_table.rb
@@ -0,0 +1,5 @@
+class Puppet::Provider::xcat_table < Puppet::Provider
+
+  commands  :chtab => 
+
+end
