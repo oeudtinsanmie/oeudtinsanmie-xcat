2edbabe86f94b0444e611f205c46fc932330d660
Douglas McClusky <drmcclus@ncsu.edu>
2014-02-17 08:19:43 -0500

    Outlined xcat_object provider superclass.  Will implement generic methods for all xcat object types here, and then create subclasses to provide each object type.

--- lib/puppet/provider/xcat_object.rb
+++ lib/puppet/provider/xcat_object.rb
@@ -5,4 +5,22 @@ class Puppet::Provider::xcat_object < Puppet::Provider
             :rmdef => '/opt/xcat/bin/rmdef',
             :chdef => '/opt/xcat/bin/chdef'
             
+  def self.instances |type| 
+    
+  end
+  
+  def exists?
+    @property_hast[:ensure] == :present
+  end
+  
+  # mk_resource_methods foreach child
+  
+  def create
+  end
+  
+  def destroy
+  end
+  
+  def flush
+  end
 end
