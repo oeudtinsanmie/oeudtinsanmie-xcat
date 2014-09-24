07c9fb9a91f933f3df2d4b2beb853d00501523b9
Douglas McClusky <drmcclus@ncsu.edu>
2014-09-24 12:01:33 -0400

    Documented class definitions.  Cleaned up spacing.

--- manifests/image.pp
+++ manifests/image.pp
@@ -1,31 +1,47 @@
-include xcat
-
+# Class: xcat::image
+#
+# This class creates an xcat_copycds resource with the given filepath, distro and architecture
+# If a url is provided, this class fetches an iso from the designated location with wget to the filepath before copycds
+#
+# Parameters:
+# [*ensure*] 
+#   - Passthrough for ensurable objects in this class
+#     Defaults to present
+# [*url*] 
+#   - If defined, the url from which to fetch isos via wget
+#     Defaults to undef
+# [*filepath*]
+#   - The location for the isos xcat_copycds with use
+# [*distro*]
+#   - Distribution of this iso
+# [*arch*]
+#   - Architecture this iso targets
 define xcat::image(
-	$ensure		= present,
-	$url		= undef,
-	$filepath,
-	$distro,
-	$arch,
+  $ensure  = present,
+  $url     = undef,
+  $filepath,
+  $distro,
+  $arch,
 ) {
-	if $url != undef {
-		wget::fetch{ $url :
-			destination => $filepath,
-			timeout => 0,
-			verbose => false,
-		} ->
-		xcat_copycds { "${distro}-${arch}" :
-			ensure  => $ensure,
-			distro  => $distro,
-			file 	=> $filepath,
-			arch 	=> $arch,
-		}
-	} 
-	else {
-		xcat_copycds { "${distro}-${arch}" :
-                        ensure 	=> $ensure,
-			distro  => $distro,
-			file 	=> $filepath,
-			arch 	=> $arch,
-		}
-	}
+  if $url != undef {
+    wget::fetch{ $url :
+      destination  => $filepath,
+      timeout      => 0,
+      verbose      => false,
+    } ->
+    xcat_copycds { "${distro}-${arch}" :
+      ensure => $ensure,
+      distro => $distro,
+      file    => $filepath,
+      arch    => $arch,
+    }
+  } 
+  else {
+    xcat_copycds { "${distro}-${arch}" :
+      ensure  => $ensure,
+      distro  => $distro,
+      file    => $filepath,
+      arch    => $arch,
+    }
+  }
 }
--- manifests/template.pp
+++ manifests/template.pp
@@ -1,27 +1,93 @@
-include xcat
-
+# Class: xcat::template
+#
+# This class defines related tmpl and pkglist files for an xcat template
+# You can use the xcat_script function to configure your own pre and postscript files in the correct filepath using the File resource 
+#
+# Parameters:
+# [*os*]
+#   - OS for which to use this template
+# [*xcatroot*] 
+#   - Root folder of xCAT
+#     Defaults to '/opt/xcat',
+# [*zerombr*] 
+#   - Zero the MBR during provisioning
+#     Defaults to true
+# [*clearpart*] 
+#   - Clear partitions during provisioning according to these arguments.  If empty, clearpart line is not added to the kickstart template
+#     Defaults to [ 'all', 'initlabel', ]
+# [*cmdline*] 
+#   - Add cmdline to kickstart template
+#     Defaults to false
+# [*partitions*]
+#   - Array of partition definition lines to add to the kickstart template
+#     May include part and raid lines, for example.  I won't document the partition syntax here.  Check out kickstart template documentation.  There are many good examples out there
+# [*keyboard*] 
+#   - Keyboard type
+#     Defaults to 'us'
+# [*lang*] 
+#   - Language
+#     Defaults to 'en_US'
+# [*bootloaderopts*] 
+#   - Options for bootloader
+#     Defaults to [],
+# [*installupgrade*] 
+#   - Install or Upgrade
+#     Defaults to 'install'
+# [*installmode*] 
+#   - Default install mode is graphical.  You can put text here to specify a text installer.
+#     Defaults to '',
+# [*firewall*] 
+#   - Arguments for the firewall line of the kickstart template.  Skips if this array is empty.
+#     Defaults to [ 'disable' ]
+# [*authconfig*] 
+#   - Arguments for the authconfig line of the kickstart template.  Skips if this array is empty. 
+#     Defaults to [ 'usershadow', 'enabledmd5', ]
+# [*selinux*] 
+#   - Arguments for the selinux line of the kickstart template.  Skips if this array is empty.
+#     Defaults to [ 'disabled', ]
+# [*mouse*] 
+#   - Mouse settings.  Skips if string is empty.
+#     Defaults to ''
+# [*skipx*] 
+#   - Whether to skip installation of XWindows support.  Adds the skipx line to the kickstart template if true.
+#     Defaults to true
+# [*resolvedeps*] 
+#   - Add --resolvedeps to the %packages line (Don't use this if it's been deprecated for the operating system version you are using)
+#     Defaults to false
+# [*pkgs*]
+#   - Array of packages to install.  Gets put in the .pkglist file for this xcat template
+# [*otheropts*] 
+#   - Any other lines you wish to be added to the kickstart template.
+#     Defaults to []
+# [*prescripts*] 
+#   - Array of prescripts to import into kickstart via xCAT.  You can use the xcat_script function to configure your own pre and postscript files in the correct filepath using the File resource 
+#     Defaults to [ "pre.${os}", ]
+# [*postscripts*] 
+#   - Array of postscripts to import into kickstart via xCAT.  You can use the xcat_script function to configure your own pre and postscript files in the correct filepath using the File resource
+#     Defaults to [ "post.${os}", ]
+#
 define xcat::template(
   $os,
-  $xcatroot = '/opt/xcat',
-  $zerombr = true,
-  $clearpart = [ 'all', 'initlabel', ],
-  $cmdline = false,
-  $partitions, # <-- add a sane list here = [],
-  $keyboard = 'us',
-  $lang = 'en_US',
+  $xcatroot       = '/opt/xcat',
+  $zerombr        = true,
+  $clearpart      = [ 'all', 'initlabel', ],
+  $cmdline        = false,
+  $partitions,    # <-- add a sane list here = [],
+  $keyboard       = 'us',
+  $lang           = 'en_US',
   $bootloaderopts = [],
   $installupgrade = 'install',
-  $installmode = '',
-  $firewall = [ 'disable' ],
-  $authconfig = [ 'usershadow', 'enabledmd5', ],
-  $selinux = [ 'disabled', ],
-  $mouse = '',
-  $skipx = true,
-  $resolvedeps = false,
-  $pkgs, # <-- add sane list here = [],
-  $otheropts = [],
-  $prescripts = undef,
-  $postscripts = undef,
+  $installmode    = '',
+  $firewall       = [ 'disable' ],
+  $authconfig     = [ 'usershadow', 'enabledmd5', ],
+  $selinux        = [ 'disabled', ],
+  $mouse          = '',
+  $skipx          = true,
+  $resolvedeps    = false,
+  $pkgs,          # <-- add sane list here = [],
+  $otheropts      = [],
+  $prescripts     = undef,
+  $postscripts    = undef,
 ) {
   if $prescripts == undef {
     $pre = [ "pre.${os}", ]
